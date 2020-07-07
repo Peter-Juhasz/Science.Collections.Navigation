d9ed571ca0542ae982ea55cf72394a4631c470a5
Juhász Péter <peter.juhasz@live.com>
2020-07-07 13:41:38 +0200

    writers

---
+++ src/System.Collections.Navigation/ListPosition.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+
+namespace System.Collections.Navigation
+{
+    public ref struct ListPosition<T>
+    {
+        public ListPosition(IList<T> buffer, int index)
+        {
+            Buffer = buffer;
+            _index = index;
+            ValidateIndex(index);
+        }
+
+        public static ListPosition<T> Create(IList<T> list, int index) => new ListPosition<T>(list, index);
+
+        public static ListPosition<T> Create(IList<T> list, Index index) => new ListPosition<T>(list, index.GetOffset(list.Count));
+
+        public readonly IList<T> Buffer { get; }
+
+        private int _index;
+        public int Index
+        {
+            get => _index;
+            set
+            {
+                ValidateIndex(value);
+                _index = value;
+            }
+        }
+
+        private bool IsStalled => Buffer.Count == 0 && Index == 0;
+        private bool IsOutOfRange => Index < 0 || Index >= Buffer.Count;
+        private bool IsHead => Buffer.Count == Index;
+
+        public T Value
+        {
+            get => Buffer[Index];
+            set => Buffer[Index] = value;
+        }
+
+        public bool CanMoveNext => Index < Buffer.Count - 1;
+        public bool CanMovePrevious => Index > 0;
+
+        public void MoveNext() => Index++;
+        public void MovePrevious() => Index--;
+
+        public void MoveToStart() => Index = 0;
+        public void MoveToEnd() => Index = Buffer.Count - 1;
+
+        public bool IsStart => Index == 0;
+        public bool IsEnd => Index == Buffer.Count - 1;
+
+        public void Offset(int delta) => Index += delta;
+
+        public T Read() => Value;
+
+        public void Write(T value) => Buffer[Index] = value;
+
+        public void WriteAndAdvance(T value)
+        {
+            Write(value);
+            if (!IsHead) MoveNext();
+        }
+
+        public void Remove()
+        {
+            Buffer.RemoveAt(Index);
+        }
+
+        public void Insert(T value)
+        {
+            Buffer.Insert(Index, value);
+        }
+
+        public void InsertAndAdvance(T value)
+        {
+            Buffer.Insert(Index, value);
+            MoveNext();
+        }
+
+        public T Peek(int delta)
+        {
+            var newIndex = Index + delta;
+            if (newIndex < 0 || newIndex >= Buffer.Count)
+            {
+                return default!;
+            }
+
+            return Buffer[newIndex];
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= Buffer.Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+        }
+    }
+
+    public static partial class Extensions
+    {
+        public static ListPosition<T> GetPositionOf<T>(this IList<T> list, int index) => ListPosition<T>.Create(list, index);
+        public static ListPosition<T> GetPositionOf<T>(this IList<T> list, Index index) => ListPosition<T>.Create(list, index);
+
+        public static ListPosition<T> GetPositionOfStart<T>(this IList<T> list) => ListPosition<T>.Create(list, 0);
+
+        public static ListPosition<T> GetPositionOfEnd<T>(this IList<T> list) => ListPosition<T>.Create(list, Math.Max(list.Count - 1, 0));
+        public static ListPosition<T> GetPositionOfHead<T>(this IList<T> list) => ListPosition<T>.Create(list, list.Count);
+    }
+}
---
+++ src/System.Collections.Navigation/SpanPosition.cs
@@ -0,0 +1,84 @@
+﻿namespace System.Collections.Navigation
+{
+    public ref struct SpanPosition<T>
+    {
+        public SpanPosition(Span<T> buffer, int index)
+        {
+            Buffer = buffer;
+            _index = index;
+            ValidateIndex(index);
+        }
+
+        public static SpanPosition<T> Create(Span<T> array, int index) => new SpanPosition<T>(array, index);
+
+        public static SpanPosition<T> Create(Span<T> array, Index index) => new SpanPosition<T>(array, index.GetOffset(array.Length));
+
+        public readonly Span<T> Buffer { get; }
+
+        private int _index;
+        public int Index
+        {
+            get => _index;
+            set
+            {
+                ValidateIndex(value);
+                _index = value;
+            }
+        }
+
+        public ref T Value => ref Buffer[Index];
+
+        public bool CanMoveNext => Index < Buffer.Length - 1;
+        public bool CanMovePrevious => Index > 0;
+
+        public void MoveNext() => Index++;
+        public void MovePrevious() => Index--;
+
+        public void MoveToStart() => Index = 0;
+        public void MoveToEnd() => Index = Buffer.Length - 1;
+
+        public bool IsStart => Index == 0;
+        public bool IsEnd => Index == Buffer.Length - 1;
+
+        public void Offset(int delta) => Index += delta;
+
+        public T Read() => Value;
+
+        public void Write(T value) => Buffer[Index] = value;
+
+        public void WriteAndAdvance(T value)
+        {
+            Write(value);
+            if (!IsEnd) MoveNext();
+        }
+
+        public T Peek(int delta)
+        {
+            var newIndex = Index + delta;
+            if (newIndex < 0 || newIndex >= Buffer.Length)
+            {
+                return default!;
+            }
+
+            return Buffer[newIndex];
+        }
+
+        private void ValidateIndex(int index)
+        {
+            if (index < 0 || index >= Buffer.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+        }
+    }
+
+    public static partial class Extensions
+    {
+        public static SpanPosition<T> GetPositionOf<T>(this Span<T> array, int index) => SpanPosition<T>.Create(array, index);
+        public static SpanPosition<T> GetPositionOf<T>(this Span<T> array, Index index) => SpanPosition<T>.Create(array, index);
+
+        public static SpanPosition<T> GetPositionOfStart<T>(this Span<T> array) => SpanPosition<T>.Create(array, 0);
+
+        public static SpanPosition<T> GetPositionOfEnd<T>(this Span<T> array) => SpanPosition<T>.Create(array, array.Length - 1);
+    }
+}
